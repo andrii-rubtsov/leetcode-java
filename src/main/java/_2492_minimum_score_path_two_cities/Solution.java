5da2c9ee15a3bc0db1e1844b83ba73ab5cd6478b
Andrii Rubtsov <andrii.rubtsov@gmail.com>
2023-03-22 20:12:03 +0200

    2492: Remove redundant intermediate variables

--- src/main/java/_2492_minimum_score_path_two_cities/Solution.java
+++ src/main/java/_2492_minimum_score_path_two_cities/Solution.java
@@ -5,37 +5,32 @@
 package _2492_minimum_score_path_two_cities;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+    boolean[] visited;
+    HashMap<Integer, HashMap<Integer, Integer>> distances;
+
     public int minScore(int n, int[][] roads) {
-        HashMap<Integer, HashMap<Integer, Integer>> distances = new HashMap<>();
+        distances = new HashMap<>();
         for (int[] road : roads) {
-            int a = road[0];
-            int b = road[1];
-            int dist = road[2];
-            HashMap<Integer, Integer> aNodes = distances.computeIfAbsent(a, k -> new HashMap<>());
-            aNodes.put(b, dist);
-            HashMap<Integer, Integer> bNodes = distances.computeIfAbsent(b, k -> new HashMap<>());
-            bNodes.put(a, dist);
+            distances.computeIfAbsent(road[0], k -> new HashMap<>()).put(road[1], road[2]);
+            distances.computeIfAbsent(road[1], k -> new HashMap<>()).put(road[0], road[2]);
         }
 
-        boolean[] visited = new boolean[n + 1];
-        return minWidth(1, visited, distances);
+        visited = new boolean[n];
+        return minWidth(1);
     }
 
-    Integer minWidth(int node, boolean[] visited, HashMap<Integer, HashMap<Integer, Integer>> distances) {
-        if (!visited[node]) {
-            visited[node] = true;
-        } else {
-            return Integer.MAX_VALUE;
-        }
-        HashMap<Integer, Integer> dist = distances.get(node);
+    Integer minWidth(int node) {
         int minValue = Integer.MAX_VALUE;
-        for (int target : dist.keySet()) {
-            int targetDist = dist.get(target);
-            minValue = Math.min(minValue, targetDist);
-            Integer minWidth = minWidth(target, visited, distances);
-            minValue = Math.min(minValue, minWidth);
+        if (!visited[node - 1]) {
+            visited[node - 1] = true;
+        } else {
+            return minValue;
+        }
+        for (Map.Entry<Integer, Integer> e : distances.get(node).entrySet()) {
+            minValue = Math.min(minValue, Math.min(e.getValue(), minWidth(e.getKey())));
         }
         return minValue;
     }
